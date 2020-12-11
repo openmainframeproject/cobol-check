64c0f646b706257ca25d333894a9a798b71945a8
Dave Nicolette <davenicolette@gmail.com>
2020-12-11 10:18:12 -0800

    Improved logic to insert test code into Cobol program

--- src/main/java/com/neopragma/cobolcheck/Generator.java
+++ src/main/java/com/neopragma/cobolcheck/Generator.java
@@ -7,7 +7,10 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.Reader;
 import java.io.Writer;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * This class merges a Test Suite (a text file) with the source of the Cobol program to be tested,
@@ -21,7 +24,19 @@ public class Generator implements Constants {
     private final Log log;
     private final TokenExtractor tokenExtractor;
 
-    private boolean inDataDivision = false;
+    private final State state = new State();
+
+    public static final String IDENTIFICATION_DIVISION = "IDENTIFICATION DIVISION";
+    public static final String ENVIRONMENT_DIVISION = "ENVIRONMENT DIVISION";
+    public static final String DATA_DIVISION = "DATA DIVISION";
+    public static final String PROCEDURE_DIVISION = "PROCEDURE DIVISION";
+    public static final String FILE_SECTION = "FILE SECTION";
+    public static final String LOCAL_STORAGE_SECTION = "LOCAL-STORAGE SECTION";
+    public static final String LINKAGE_SECTION = "LINKAGE SECTION";
+    public static final String WORKING_STORAGE_SECTION = "WORKING-STORAGE SECTION";
+
+    private boolean workingStorageTestCodeHasBeenInserted = false;
+    private String workingStorageHeader = fixedLength("       WORKING-STORAGE SECTION.");
 
     public Generator(
             Messages messages,
@@ -85,6 +100,18 @@ public class Generator implements Constants {
             String sourceLine,
             Reader cobolSourceIn,
             Writer testSourceOut) throws IOException {
+
+        if (hasToken(tokens, DATA_DIVISION)) enteringDataDivision();
+        if (hasToken(tokens, PROCEDURE_DIVISION)) {
+            enteringProcedureDivision();
+            if (workingStorageTestCodeHasBeenInserted == false) {
+                testSourceOut.write(workingStorageHeader);
+                testSourceOut.write(fixedLength(
+                        "      ***** insert working-storage code here *****"));
+            }
+
+        }
+        if (hasToken(tokens, WORKING_STORAGE_SECTION)) enteringWorkingStorageSection();
     }
 
     private void processingAfterEchoingTheSourceLineToTheOutput(
@@ -93,12 +120,13 @@ public class Generator implements Constants {
             Reader cobolSourceIn,
             Writer testSourceOut) throws IOException {
 
-        if (hasToken(tokens, "WORKING-STORAGE SECTION")) {
+        if (hasToken(tokens, WORKING_STORAGE_SECTION)) {
             testSourceOut.write(fixedLength(
                     "      ***** insert working-storage code here *****"));
+            workingStorageTestCodeHasBeenInserted = true;
         }
 
-        if (hasToken(tokens, "PROCEDURE DIVISION")) {
+        if (hasToken(tokens, PROCEDURE_DIVISION)) {
             testSourceOut.write(fixedLength(
                     "      ***** insert procedure division code here *****"));
         }
@@ -106,6 +134,18 @@ public class Generator implements Constants {
 
     private boolean hasToken(List<String> tokens, String tokenValue) {
         return tokens.size() > 0 && tokens.contains(tokenValue);
+    }
+
+    private void enteringDataDivision() {
+        state.flags.get(DATA_DIVISION).set();
+    }
+
+    private void enteringProcedureDivision() {
+        state.flags.get(PROCEDURE_DIVISION).set();
+    }
+
+    private void enteringWorkingStorageSection() {
+        state.flags.get(WORKING_STORAGE_SECTION).set();
     }
 
 
@@ -117,6 +157,94 @@ public class Generator implements Constants {
      */
     private String fixedLength(String sourceLine) {
         return String.format("%1$-80s", sourceLine) + NEWLINE;
+    }
+
+
+    class State {
+        private Map<String, Flag> flags;
+
+        public State() {
+            flags = new HashMap<>();
+            flags.put(FILE_SECTION, new Flag());
+            flags.put(LINKAGE_SECTION, new Flag());
+            flags.put(LOCAL_STORAGE_SECTION, new Flag());
+            flags.put(WORKING_STORAGE_SECTION, new Flag());
+            mutuallyExclusiveFlagsFor(FILE_SECTION,
+                    LINKAGE_SECTION, LOCAL_STORAGE_SECTION, WORKING_STORAGE_SECTION);
+            mutuallyExclusiveFlagsFor(LINKAGE_SECTION,
+                    FILE_SECTION, LOCAL_STORAGE_SECTION, WORKING_STORAGE_SECTION);
+            mutuallyExclusiveFlagsFor(LOCAL_STORAGE_SECTION,
+                    LINKAGE_SECTION, FILE_SECTION, WORKING_STORAGE_SECTION);
+            mutuallyExclusiveFlagsFor(WORKING_STORAGE_SECTION,
+                    LINKAGE_SECTION, LOCAL_STORAGE_SECTION, FILE_SECTION);
+
+            flags.put(IDENTIFICATION_DIVISION, new Flag());
+
+            flags.put(ENVIRONMENT_DIVISION, new Flag());
+
+            flags.put(DATA_DIVISION, new Flag());
+            dependentFlagsFor(DATA_DIVISION,
+                    FILE_SECTION, LINKAGE_SECTION, LOCAL_STORAGE_SECTION, WORKING_STORAGE_SECTION);
+
+            flags.put(PROCEDURE_DIVISION, new Flag());
+
+            mutuallyExclusiveFlagsFor(IDENTIFICATION_DIVISION,
+                    DATA_DIVISION, ENVIRONMENT_DIVISION, PROCEDURE_DIVISION);
+            mutuallyExclusiveFlagsFor(ENVIRONMENT_DIVISION,
+                    IDENTIFICATION_DIVISION, DATA_DIVISION, PROCEDURE_DIVISION);
+            mutuallyExclusiveFlagsFor(DATA_DIVISION,
+                    IDENTIFICATION_DIVISION, ENVIRONMENT_DIVISION, PROCEDURE_DIVISION);
+            mutuallyExclusiveFlagsFor(PROCEDURE_DIVISION,
+                    IDENTIFICATION_DIVISION, ENVIRONMENT_DIVISION, DATA_DIVISION);
+
+
+        }
+
+        private void mutuallyExclusiveFlagsFor(String token, String... mutuallyExclusiveTokens) {
+            List<Flag> mutuallyExclusiveFlags = new ArrayList<>();
+            for (String mutuallyExclusiveToken : mutuallyExclusiveTokens) {
+                mutuallyExclusiveFlags.add(flags.get(mutuallyExclusiveToken));
+            }
+            flags.get(token).setMutuallyExclusiveFlags(mutuallyExclusiveFlags);
+        }
+        private void dependentFlagsFor(String token, String... dependentTokens) {
+            List<Flag> dependentFlags = new ArrayList<>();
+            for (String dependentToken : dependentTokens) {
+                dependentFlags.add(flags.get(dependentToken));
+            }
+            flags.get(token).setDependentFlags(dependentFlags);
+        }
+    }
+
+    class Flag {
+        private boolean state = false;
+        private List<Flag> mutuallyExclusiveFlags;
+        private List<Flag> dependentFlags;
+        public Flag() {
+            this.mutuallyExclusiveFlags = new ArrayList<>();
+            this.dependentFlags = new ArrayList<>();
+        }
+        public void setMutuallyExclusiveFlags(List<Flag> mutuallyExclusiveFlags) {
+            this.mutuallyExclusiveFlags = mutuallyExclusiveFlags;
+        }
+        public void setDependentFlags(List<Flag> dependentFlags) {
+            this.dependentFlags = dependentFlags;
+        }
+        public boolean isSet() {
+            return state;
+        }
+        public void set() {
+            state = true;
+            for (Flag flag : mutuallyExclusiveFlags) {
+                flag.unset();
+            }
+        }
+        public void unset() {
+            state = false ;
+            for (Flag flag : dependentFlags) {
+                flag.unset();
+            }
+        }
     }
 
 }
--- src/test/java/com/neopragma/cobolcheck/GeneratorTestCodeInsertionTest.java
+++ src/test/java/com/neopragma/cobolcheck/GeneratorTestCodeInsertionTest.java
@@ -14,33 +14,52 @@ import java.io.StringWriter;
 public class GeneratorTestCodeInsertionTest implements Constants {
 
     private Generator generator;
-    private Messages messages = new Messages();
-    private Log log = new Log();
-    private TokenExtractor tokenExtractor = new StringTokenizerExtractor(messages);
+    private final Messages messages = new Messages();
+    private final Log log = new Log();
+    private final TokenExtractor tokenExtractor = new StringTokenizerExtractor(messages);
 
     @Mock
     Reader mockTestSuite;
 
-    private String[] simple1CobolSource = new String[] {
-        " IDENTIFICATION DIVISION.",
-        " PROGRAM-ID. SIMPLE1.",
-        " ENVIRONMENT DIVISION.",
-        "       DATA DIVISION.",
-        "       WORKING-STORAGE SECTION.",
-        "       01  W-GROUP-1.",
-        "           05  W-FIELD-1 PIC X(03) VALUE \"FOO\".",
-        "           05  FILLER    PIC X(01) VALUE \"-\".",
-        "           05  W-FIELD-2 PIC X(03) VALUE \"XXX\".",
-        "       PROCEDURE DIVISION.",
-        "       100-APPEND-TEXT.",
-        "           MOVE \"ZZZ\" TO W-FIELD-2",
-        "           DISPLAY W-GROUP-1 UPON CONSOLE.",
-        "           .",
-        "       200-NOT-UNDER-TEST.",
-        "           DISPLAY \"Should not see this.\" UPON CONSOLE",
-        "           ."
+    private final String[] cobolSourceWithWorkingStorage = new String[] {
+            " IDENTIFICATION DIVISION.",
+            " PROGRAM-ID. SIMPLE1.",
+            " ENVIRONMENT DIVISION.",
+            " DATA DIVISION.",
+            " LOCAL-STORAGE SECTION.",
+            " 01  W-GROUP-1.",
+            "     05  W-FIELD-1 PIC X(03) VALUE \"FOO\".",
+            "     05  FILLER    PIC X(01) VALUE \"-\".",
+            "     05  W-FIELD-2 PIC X(03) VALUE \"XXX\".",
+            " PROCEDURE DIVISION.",
+            " 100-APPEND-TEXT.",
+            "     MOVE \"ZZZ\" TO W-FIELD-2",
+            "     DISPLAY W-GROUP-1 UPON CONSOLE.",
+            "     .",
+            " 200-NOT-UNDER-TEST.",
+            "     DISPLAY \"Should not see this.\" UPON CONSOLE",
+            "     ."
     };
 
+    private final String[] cobolSourceWithoutWorkingStorage = new String[] {
+            " IDENTIFICATION DIVISION.",
+            " PROGRAM-ID. SIMPLE1.",
+            " ENVIRONMENT DIVISION.",
+            " DATA DIVISION.",
+            " LOCAL-STORAGE SECTION.",
+            " 01  W-GROUP-1.",
+            "     05  W-FIELD-1 PIC X(03) VALUE \"FOO\".",
+            "     05  FILLER    PIC X(01) VALUE \"-\".",
+            "     05  W-FIELD-2 PIC X(03) VALUE \"XXX\".",
+            " PROCEDURE DIVISION.",
+            " 100-APPEND-TEXT.",
+            "     MOVE \"ZZZ\" TO W-FIELD-2",
+            "     DISPLAY W-GROUP-1 UPON CONSOLE.",
+            "     .",
+            " 200-NOT-UNDER-TEST.",
+            "     DISPLAY \"Should not see this.\" UPON CONSOLE",
+            "     ."
+    };
 
     @BeforeEach
     public void commonSetup() {
@@ -49,7 +68,7 @@ public class GeneratorTestCodeInsertionTest implements Constants {
 
     @Test
     public void it_inserts_test_copybooks_in_the_right_places() {
-        StringReader cobolSourceIn = makeCobolSourceProgram(simple1CobolSource);
+        StringReader cobolSourceIn = makeCobolSourceProgram(cobolSourceWithoutWorkingStorage);
         StringWriter testSourceOut = new StringWriter();
         generator.mergeTestSuite(mockTestSuite, cobolSourceIn, testSourceOut);
 
