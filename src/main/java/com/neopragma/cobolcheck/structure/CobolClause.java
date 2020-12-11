fe6f957e18c86fe30af6f73b00c276ba3ab5e154
Dave Nicolette <davenicolette@gmail.com>
2020-12-11 07:42:14 -0800

    Removed the 'structure' package; not needed

--- src/main/java/com/neopragma/cobolcheck/structure/CobolClause.java
+++
@@ -1,7 +0,0 @@
-package com.neopragma.cobolcheck.structure;
-
-public interface CobolClause {
-    String getName();
-    boolean isMandatory();
-    default String getArea() { return "A"; }
-}
--- src/main/java/com/neopragma/cobolcheck/structure/CobolParagraph.java
+++
@@ -1,7 +0,0 @@
-package com.neopragma.cobolcheck.structure;
-
-public interface CobolParagraph {
-
-    String getName();
-    boolean isMandatory();
-}
