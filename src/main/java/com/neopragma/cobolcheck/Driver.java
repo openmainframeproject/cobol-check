22427d6b58840a4d7e1c114d6cc7dc41737fea60
Dave Nicolette <davenicolette@gmail.com>
2021-01-01 08:50:45 -0800

    item #10 concatenate multiple test suite input files to feed to Generator

--- src/main/java/com/neopragma/cobolcheck/Driver.java
+++ src/main/java/com/neopragma/cobolcheck/Driver.java
@@ -15,6 +15,10 @@ limitations under the License.
 */
 package com.neopragma.cobolcheck;
 
+import com.neopragma.cobolcheck.exceptions.ConcatenatedTestSuiteIOException;
+import com.neopragma.cobolcheck.exceptions.TestSuiteInputFileNotFoundException;
+
+import java.io.*;
 import java.util.Locale;
 
 /**
@@ -25,10 +29,16 @@ import java.util.Locale;
  */
 public class Driver implements Constants, StringHelper {
 
+    public static final String TEST_SUITE_PATH_OPTION = "test-suite-path";
+    public static final String TEST_SUITE_PATH_CONFIG_KEY = "test.suite.path";
     private final Config config;
     private static Messages messages;
     private final GetOpt options;
-    private static final String optionSpec = "c:l:h --long config-file:,log-level:,help";
+    private Generator generator;
+    private Reader testSuite;
+    private Reader cobolSourceIn;
+    private Writer testSourceOut;
+    private static final String optionSpec = "c:l:t:h --long config-file:,log-level:,test-suite-path:,help";
     private String configFileFromCommandLine = EMPTY_STRING;
 
     private final String[] helpText = {
@@ -37,7 +47,10 @@ public class Driver implements Constants, StringHelper {
             "      Java properties file containing configuration settings for this run.",
             "      Default: ./config.properties",
             "  -l|--log-level off|fatal|info|warn|error|debug|trace",
-            "      Default: INFO"
+            "      Default: INFO",
+            "  -t|--test-suite-path absolute-or-relative-path[:absolute-or-relative-path[...]]",
+            "      Location(s) of test suite input file(s) for this run. Values specified here are prefixed",
+            "      to those specified in the config file as test.suite.path (if any)."
     };
 
     public Driver(
@@ -56,8 +69,81 @@ public class Driver implements Constants, StringHelper {
             return;
         }
         initialize();
+        concatenateTestSuitesForThisRun();
+        mergeTestSuiteIntoTheProgramUnderTest();
 
         Log.info(messages.get("INF004"));        // We are terminating
+    }
+
+    /**
+     * Multiple test suite files may be input to Cobol Check. Their absolute or relative paths are specified in the
+     * config file under key test.suite.path and/or on the command line via option -t or --test-suite-path. Entries
+     * specified on the command line are processed first and those specified in the config file second. There is no
+     * default path or test suite filename. If nothing is specified, we throw TestSuiteInputFileNotFoundException.
+     *
+     * The various test suite input files are concatenated into a single file for the Generator to process.
+     * In case of an IOException on this file, we throw ConcatenatedTestSuiteIOException and provide info to
+     * help correct the error.
+     *
+     * @throws ConcatenatedTestSuiteIOException
+     * @throws TestSuiteInputFileNotFoundException
+     */
+    //TODO: Extract this to a separate class
+    void concatenateTestSuitesForThisRun() {
+        //TODO: Make this a config setting
+        String concatenatedTestSuiteFileName = "./ALLTESTS";
+        FileWriter concatenatedTestSuitesWriter;
+        try {
+            concatenatedTestSuitesWriter = new FileWriter(concatenatedTestSuiteFileName);
+        } catch (IOException concatenatedTestSuitesException) {
+            throw new ConcatenatedTestSuiteIOException(
+                    messages.get("ERR012", concatenatedTestSuiteFileName),
+                    concatenatedTestSuitesException);
+        }
+        String testSuitePathsFromCommandLine = EMPTY_STRING;
+        if (options.isSet(TEST_SUITE_PATH_OPTION)) {
+            testSuitePathsFromCommandLine = options.getValueFor(TEST_SUITE_PATH_OPTION);
+        }
+        String testSuitePathsFromConfig =
+                config.getString(TEST_SUITE_PATH_CONFIG_KEY, Constants.EMPTY_STRING);
+        StringBuffer testSuitePaths = new StringBuffer();
+        if (notBlank(testSuitePathsFromCommandLine)) {
+            testSuitePaths.append(testSuitePathsFromCommandLine);
+        }
+        if (notBlank(testSuitePathsFromConfig)) {
+            testSuitePaths.append(COLON);
+            testSuitePaths.append(testSuitePathsFromConfig);
+        }
+        String[] testSuitePathList = testSuitePaths.toString().split(COLON);
+
+        String line = EMPTY_STRING;
+        for (String pathname : testSuitePathList) {
+            try {
+                Log.info(messages.get("INF007", pathname, concatenatedTestSuiteFileName));
+                testSuite = new FileReader(pathname);
+            } catch (FileNotFoundException testSuiteException) {
+                throw new TestSuiteInputFileNotFoundException(
+                        messages.get("ERR011", pathname),
+                        testSuiteException);
+            }
+
+        }
+
+        cobolSourceIn = new StringReader("MINIMAL-BEFORE.CBL");
+        testSourceOut = new StringWriter();
+
+    }
+
+    void mergeTestSuiteIntoTheProgramUnderTest() {
+        generator = new Generator(messages,
+                new StringTokenizerExtractor(messages),
+                new KeywordExtractor(),
+                config);
+        generator.mergeTestSuite(
+                testSuite,
+                cobolSourceIn,
+                testSourceOut
+        );
     }
 
     void initialize() {
