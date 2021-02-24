ede6d98500fa6840cb38f87a6e19133f7fb0cd97
Dave Nicolette <davenicolette@gmail.com>
2021-02-24 11:39:03 -0800

    closes #101 Windows test runner script

---
+++ src/main/java/com/neopragma/cobolcheck/WindowsProcessLauncher.java
@@ -0,0 +1,87 @@
+/*
+Copyright 2020 David Nicolette
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package com.neopragma.cobolcheck;
+
+import com.neopragma.cobolcheck.exceptions.PossibleInternalLogicErrorException;
+
+import java.io.IOException;
+
+/**
+ * Launch a script to compile and execute a generated test program (Cobol)
+ *
+ * @author Dave Nicolette
+ * @since 1.5
+ */
+public class WindowsProcessLauncher implements ProcessLauncher, StringHelper {
+
+    private Config config;
+    private Messages messages;
+
+    public WindowsProcessLauncher(Config config) {
+        this.config = config;
+        this.messages = config.getMessages();
+    }
+
+    @Override
+    public Process run(String programName) {
+        if (Log.level() == LogLevel.DEBUG) {
+            System.out.println("Entering WindowsProcessLauncher.run() method, programName is <" + programName + ">");
+        }
+        if (isBlank(programName)) {
+            Log.error(messages.get("ERR020"));
+            throw new PossibleInternalLogicErrorException(messages.get("ERR020"));
+        }
+        String processConfigKey = "windows" + Constants.PROCESS_CONFIG_KEY;
+        String scriptName = config.getString(processConfigKey);
+
+        if (isBlank(scriptName)) {
+            Log.error(messages.get("ERR021", processConfigKey));
+            throw new PossibleInternalLogicErrorException(messages.get("ERR021", processConfigKey));
+        }
+        String scriptDirectory = config.getString(Constants.COBOLCHECK_SCRIPT_DIRECTORY_CONFIG_KEY,
+                Constants.DEFAULT_COBOLCHECK_SCRIPT_DIRECTORY);
+        if (isBlank(scriptDirectory)) {
+            Log.error(messages.get("ERR022", Constants.COBOLCHECK_SCRIPT_DIRECTORY_CONFIG_KEY));
+            throw new PossibleInternalLogicErrorException(messages.get("ERR022", Constants.COBOLCHECK_SCRIPT_DIRECTORY_CONFIG_KEY));
+        }
+
+        if (!scriptDirectory.endsWith(Constants.FILE_SEPARATOR)) {
+            scriptDirectory += Constants.FILE_SEPARATOR;
+        }
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        processBuilder.command(scriptDirectory + scriptName, programName);
+        processBuilder.inheritIO();
+        Process process = null;
+        StringBuilder processArguments = new StringBuilder();
+        String delim = Constants.EMPTY_STRING;
+        for (String argument : processBuilder.command()) {
+            processArguments.append(delim);
+            processArguments.append(argument);
+            delim = Constants.COMMA;
+        }
+        Log.info(messages.get("INF008", processArguments.toString()));
+        try {
+            process = processBuilder.start();
+        } catch (IOException processBuilderException) {
+            Log.error(messages.get("ERR023", processArguments.toString()));
+            throw new PossibleInternalLogicErrorException(messages.get("ERR023",
+                    processArguments.toString()), processBuilderException);
+        }
+        return process;
+    }
+
+
+}
--- src/test/java/com/neopragma/cobolcheck/ProcessLauncherIT.java
+++ src/test/java/com/neopragma/cobolcheck/ProcessLauncherIT.java
@@ -5,22 +5,31 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-public class LinuxProcessLauncherIT {
+public class ProcessLauncherIT {
 
     Messages messages = new Messages();
     Config config;
     private ProcessLauncher launcher;
+    private String testProgramName;
 
     @BeforeEach
     public void commonSetup() {
         config = new Config(messages);
         config.load("testconfig.properties");
-        launcher = new LinuxProcessLauncher(config);
+        switch (PlatformLookup.get()) {
+            case WINDOWS:
+                launcher = new WindowsProcessLauncher(config);
+                testProgramName = "LAUNCHTESTT";
+                break;
+            case LINUX:
+                launcher = new LinuxProcessLauncher(config);
+                testProgramName = "LAUNCHTESTT.CBL";
+        }
     }
 
     @Test
     public void it_launches_process_to_compile_and_run_a_test_program() throws InterruptedException {
-        Process process = launcher.run("LAUNCHTESTT.CBL");
+        Process process = launcher.run(testProgramName);
         assertNotNull(process, "Process was not started.");
         int exitCode = process.waitFor();
         assertTrue(exitCode < 5, "Exit 0 means all tests passed, 4 means some failed but process worked");
