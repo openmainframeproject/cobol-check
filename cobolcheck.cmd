93f1724b261c745852f49960403d6bab9156d39d
Dave Nicolette <davenicolette@gmail.com>
2021-02-24 15:39:59 -0800

    closes #101 prepare Windows distribution
    Signed-off-by: Christopher Morgan <cmorgan@mercurial-solutions.com>

---
+++ cobolcheck.cmd
@@ -0,0 +1,10 @@
+echo off
+java -jar bin\cobol-check-0.0.1.jar %* 
+:loop
+cobc -x %2T.CBL && %2T.exe 
+shift
+set pname=%2
+if "%pname%"=="" goto end
+if "%pname:~0,1%"=="-" goto end 
+goto loop     
+:end
