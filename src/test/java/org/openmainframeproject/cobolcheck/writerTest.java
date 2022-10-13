5ec3c9384938c67c7b752507fec6f6ac9b091e00
Søren Andersen <san@ubi.dk>
2022-10-13 14:51:07 +0200

    Update writerTest.java
    Signed-off-by: Søren Andersen <san@ubi.dk>

--- src/test/java/org/openmainframeproject/cobolcheck/writerTest.java
+++ src/test/java/org/openmainframeproject/cobolcheck/writerTest.java
@@ -12,7 +12,7 @@ import org.mockito.Mockito;
 import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
-
+//san
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class writerTest {
