1ce2140d354f69dc0a1d69d45585a1782adcc406
Akash Kumar <meakash7902@gmail.com>
2023-08-30 21:41:15 +0530

    Add exception class for unmock call statement
    Signed-off-by: Akash Kumar <meakash7902@gmail.com>

---
+++ src/main/java/org/openmainframeproject/cobolcheck/exceptions/UnMockedCallStatementException.java
@@ -0,0 +1,7 @@
+package org.openmainframeproject.cobolcheck.exceptions;
+
+public class UnMockedCallStatementException extends RuntimeException {
+    public UnMockedCallStatementException(String message) {
+        super(message);
+    }
+}
