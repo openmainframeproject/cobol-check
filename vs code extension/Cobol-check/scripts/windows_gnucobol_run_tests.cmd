6c84461888cc525fe7dc73ff7294490454ef1ffd
Simon Sobisch <simonsobisch@web.de>
2023-09-18 22:37:02 +0200

    vscode extension: updates to calling external processes
    * cobc: let the compiler execute the test binary
    * cobol-check: use whatever version is in bin

--- vs code extension/Cobol-check/scripts/windows_gnucobol_run_tests.cmd
+++ vs code extension/Cobol-check/scripts/windows_gnucobol_run_tests.cmd
@@ -1,8 +1,9 @@
 @echo off
-rem Windows - compile and execute a test Cobol program
-rem
-rem  Assumptions:
-rem
-rem GnuCOBOL 3.+ is installed and on the path. Its executable or alias or symlink is named "cobc".
+:: Windows - compile and execute a test Cobol program
+::
+::  Assumptions:
+::
+:: GnuCOBOL 2.2 or later is installed and on the path.
+:: Its executable or alias or symlink is named "cobc".
 
-cobc -x %* && %~n1
+cobc -xj %* && %~n1
