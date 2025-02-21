517a446f4aa8673977f8cad4fb4e27e5c4e7ba81
Thomas Nellemann Kramer <tnp@bankdata.dk>
2025-02-21 13:40:41 +0100

    import removed

--- src/main/java/org/openmainframeproject/cobolcheck/services/cobolLogic/replace/ReplaceSet.java
+++ src/main/java/org/openmainframeproject/cobolcheck/services/cobolLogic/replace/ReplaceSet.java
@@ -1,6 +1,5 @@
 package org.openmainframeproject.cobolcheck.services.cobolLogic.replace;
 
-import jdk.nashorn.internal.runtime.regexp.joni.Regex;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
