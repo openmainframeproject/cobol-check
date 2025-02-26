b1b4ac14ca1a5717c11bee6d8c2a0d342dc6f211
Thomas Christensen <tac@bankdata.dk>
2025-02-26 12:57:38 +0100

    Bump - package version

--- vs-code-extension/client/src/test/index.ts
+++ vs-code-extension/client/src/test/index.ts
@@ -1,6 +1,5 @@
+import { glob } from 'glob';
 import * as path from 'path';
-import * as Mocha from 'mocha';
-import * as glob from 'glob';
 
 export function run(): Promise<void> {
 	// Create the mocha test
