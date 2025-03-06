481c2ba83263c399b28492ff7e73a5f9fe13bc86
Thomas Nellemann Kramer <tnp@bankdata.dk>
2025-03-06 14:04:38 +0100

    Replace
    Updated the Replace class to work relative to the placement of the REPLACE statements in the COBOL code.

--- src/test/java/org/openmainframeproject/cobolcheck/services/cobolLogic/replace/ReplaceSetTest.java
+++ src/test/java/org/openmainframeproject/cobolcheck/services/cobolLogic/replace/ReplaceSetTest.java
@@ -10,19 +10,19 @@ public class ReplaceSetTest {
     public void testReplaceSet() {
 
         // Create a Replace set
-        ReplaceSet replaceSet = new ReplaceSet("REPLACE", "REPLACED", false, false);
+        ReplaceSet replaceSet = new ReplaceSet("REPLACE", "REPLACED", false, false,0,0);
         assertEquals("REPLACE", replaceSet.getFrom());
         assertEquals("REPLACED", replaceSet.getTo());
         assertFalse(replaceSet.isTrailing());
         assertFalse(replaceSet.isLeading());
 
-        replaceSet = new ReplaceSet("BRUCE", "CAITLYN", true, false);
+        replaceSet = new ReplaceSet("BRUCE", "CAITLYN", true, false,0,0);
         assertEquals("BRUCE", replaceSet.getFrom());
         assertEquals("CAITLYN", replaceSet.getTo());
         assertTrue(replaceSet.isTrailing());
         assertFalse(replaceSet.isLeading());
 
-        replaceSet = new ReplaceSet("JOHNNY", "JAY", false, true);
+        replaceSet = new ReplaceSet("JOHNNY", "JAY", false, true,0,0);
         assertEquals("JOHNNY", replaceSet.getFrom());
         assertEquals("JAY", replaceSet.getTo());
         assertFalse(replaceSet.isTrailing());
@@ -31,17 +31,17 @@ public class ReplaceSetTest {
         // Having both trailing and leading set to true is not allowed
         // This should throw an exception
         // Not possible via constructor
-        assertThrows(IllegalArgumentException.class, () -> new ReplaceSet("JOHNNY", "JAY", true, true));
+        assertThrows(IllegalArgumentException.class, () -> new ReplaceSet("JOHNNY", "JAY", true, true,0,0));
         // not possible via setter
-        ReplaceSet replaceSetLeading = new ReplaceSet("REPLACE", "REPLACED", false, true);
+        ReplaceSet replaceSetLeading = new ReplaceSet("REPLACE", "REPLACED", false, true,0,0);
         assertThrows(IllegalArgumentException.class, () -> replaceSetLeading.setTrailing(true));
-        ReplaceSet replaceSetTrailing = new ReplaceSet("REPLACE", "REPLACED", true, false);
+        ReplaceSet replaceSetTrailing = new ReplaceSet("REPLACE", "REPLACED", true, false,0,0);
         assertThrows(IllegalArgumentException.class, () -> replaceSetTrailing.setLeading(true));
     }
 
     @Test
     public void testReplaceInline() {
-        ReplaceSet replaceSet = new ReplaceSet("JOHNNY", "JAY", false, false);
+        ReplaceSet replaceSet = new ReplaceSet("JOHNNY", "JAY", false, false,0,0);
         String from = "Johnny is behind the iconic late-night desk.";
         String expected = "JAY is behind the iconic late-night desk.";
         assertEquals(expected, replaceSet.replaceInline(from));
@@ -49,7 +49,7 @@ public class ReplaceSetTest {
 
     @Test
     public void testReplaceInlineTrailing() {
-        ReplaceSet replaceSet = new ReplaceSet("night", "day", true, false);
+        ReplaceSet replaceSet = new ReplaceSet("night", "day", true, false,0,0);
         String from = "Johnny is behind the iconic late-night desk.";
         String expected = "Johnny is behind the iconic late-day desk.";
         assertEquals(expected, replaceSet.replaceInline(from));
@@ -61,13 +61,11 @@ public class ReplaceSetTest {
         from = "Johnny is working day and night behind the iconic desk.";
         expected = "Johnny is working day and night behind the iconic desk.";
         assertEquals(expected, replaceSet.replaceInline(from));
-
-
     }
 
     @Test
     public void testReplaceInlineLeading() {
-        ReplaceSet replaceSet = new ReplaceSet("late", "early", false, true);
+        ReplaceSet replaceSet = new ReplaceSet("late", "early", false, true,0,0);
         String from = "Johnny is behind the iconic late-night desk.";
         String expected = "Johnny is behind the iconic early-night desk.";
         assertEquals(expected, replaceSet.replaceInline(from));
@@ -79,5 +77,27 @@ public class ReplaceSetTest {
         from = "Johnny is working early and late behind the iconic desk.";
         expected = "Johnny is working early and late behind the iconic desk.";
         assertEquals(expected, replaceSet.replaceInline(from));
+    }
+
+    @Test
+    public void testReplaceInlinewithlinenumbering() {
+        ReplaceSet replaceSet = new ReplaceSet("late", "early", false, true,10,100);
+        String from = "Johnny is behind the iconic late-night desk.";
+        String expected = "Johnny is behind the iconic early-night desk.";
+        // unchanged as line number is not in the range
+        assertEquals(from, replaceSet.replaceInline(from,5));
+        // changed as line number is in the range
+        assertEquals(expected, replaceSet.replaceInline(from,15));
+    }
+
+    @Test
+    public void testReplaceinLineWithLinenumberingLastReplaceSetHasZeroAsUntilSourceLine() {
+        ReplaceSet replaceSet = new ReplaceSet("late", "early", false, true,10,0);
+        String from = "Johnny is behind the iconic late-night desk.";
+        String expected = "Johnny is behind the iconic early-night desk.";
+        // unchanged as line number is not in the range
+        assertEquals(from, replaceSet.replaceInline(from,5));
+        // changed as line number is in the range
+        assertEquals(expected, replaceSet.replaceInline(from,15));
     }
 }
